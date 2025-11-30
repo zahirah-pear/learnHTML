4270e73bcea2be9d5fd8def044aa2f952d335caa
zahirah-pear <malikanashitazahirah@gmail.com>
2025-11-30 11:57:00 +0700

    projectagung

---
+++ Javascript/projectagung/app2.js
@@ -0,0 +1,9 @@
+const hasChild = false;
+const isStudent = true;
+const isWorker = false;
+const canRideBike = true;
+const hasPet = false; 
+
+const isTall = true;
+const canCook = true;
+const hasSibling = true;
---
+++ Javascript/projectagung/app3.js
@@ -0,0 +1,18 @@
+const hasPassport = false;
+
+if (hasPassport) {
+    console.log("Kamu boleh ke luar negeri");
+}
+else {
+    console.log("Kamu tidak boleh ke luar negeri");
+}
+
+const isSolsOut = true;
+
+if (isSolsOut) {
+    console.log("Produk sudah habis");
+}
+
+else {
+    console.log("Produk masih tersedia");
+}
