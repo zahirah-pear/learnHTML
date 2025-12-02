9da82063be02fd3df5cd5fc01e25f2313c6d6c50
zahirah-pear <malikanashitazahirah@gmail.com>
2025-12-02 23:13:54 +0700

    projectagung

---
+++ Javascript/projectagung/app6object.js
@@ -0,0 +1,12 @@
+const person = {
+    name : "Yanto",
+    age : 24,
+    yearBorn : 2001,
+    hasJob : true,
+};
+
+const text = person.name + ' lahir pada tahun ' + person.yearBorn + "."
+console.log(person.name + ' lahir pada tahun ' + person.yearBorn + ".");
+console.log(text);
+
+//atau console.log(person.name);
